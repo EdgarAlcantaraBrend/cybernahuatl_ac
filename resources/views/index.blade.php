3ad8e0e0f4ba413e486fcd74dd95ba7b61db0f34
itzel <itzelmartinez455@gmail.com>
2022-10-05 15:54:12 -0500

    actualizacion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,119 +2,45 @@
 
 @section('content')
 
-<!-- Page Wrap -->
-<div class="page-wrap">
-
-  <div id="home-slider">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide swiper-slide-one">
-          <div class="swiper-image" data-swiper-parallax-y="-20%">
-            <div class="swiper-image-inner swiper-image-left swiper-image-one">
-              <h1>A <span class="emphasis">Breath</span>. <br><span>Of Fresh Air.</span></h1>
-              <p>Chapter I, page XV</p>
-            </div>
-          </div>
-          <div class="swiper-image" data-swiper-parallax-y="35%">
-            <div class="swiper-image-inner swiper-image-right swiper-image-two">
-              <p class="paragraph">
-                A Prophet sat in the market-place and told the fortunes of all who cared to engage his services. Suddenly there came running up one who told him that his house had been broken into by thieves, and that they had made off with everything they could lay
-                hands on.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="swiper-slide swiper-slide-two">
-          <div class="swiper-image" data-swiper-parallax-y="-20%">
-            <div class="swiper-image-inner swiper-image-left swiper-image-three">
-              <h1>The <span class="emphasis">Drop</span>. <br><span>Of Eternal life.</span></h1>
-              <p>Chapter II, page VII</p>
-            </div>
-          </div>
-          <div class="swiper-image" data-swiper-parallax-y="35%">
-            <div class="swiper-image-inner swiper-image-right swiper-image-four">
-              <p class="paragraph">
-                A thirsty Crow found a Pitcher with some water in it, but so little was there that, try as she might, she could not reach it with her beak, and it seemed as though she would die of thirst within sight of the remedy.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="swiper-slide swiper-slide-three">
-          <div class="swiper-image" data-swiper-parallax-y="-20%">
-            <div class="swiper-image-inner swiper-image-left swiper-image-five">
-              <h1>A <span class="emphasis">Sense</span>. <br><span>Of Things to Come.</span></h1>
-              <p>Chapter III, page XI</p>
-            </div>
-          </div>
-          <div class="swiper-image" data-swiper-parallax-y="35%">
-            <div class="swiper-image-inner swiper-image-right swiper-image-six">
-              <p class="paragraph">
-                Every man carries Two Bags about with him, one in front and one behind, and both are packed full of faults. The Bag in front contains his neighbours’ faults, the one behind his own. Hence it is that men do not see their own faults, but never fail to see
-                those of others.
-              </p>
-            </div>
-          </div>
-        </div>
-
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </div>
-      <div class="swiper-pagination"></div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
     </div>
   </div>
-
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
 
-	
-<main>
-  <section class="hero" >
-    <div class="hero-txt">
-      <p class="text-white">
-        <h1 class="text-white">En CyberNahualt encontraras cursos para toda la famila acerca 
-          del idioma Nahualt
-        </h1>
-      </p>
-    </div>
-  </section>
-  
-  <section class="aprendizaje mt-4" >
-    <h2 class="title-c">¿Porqué te encantará aprender Nahualt?</h2>
-    <div class="contenedor">
-      <article class="card">
-        <img src="" class="card-img-top" alt="..." height="180;" style="background-color: aqua">
-        <div class="card-txt text-justify">
-          <h6>En el náhuatl, en la lengua y en la cosmovisión, el ser humano no es el 
-            centro, sino que es parte de un todo, como son los animales, las plantas, todos los seres vivos.
-            Entonces cuando alguien aprende náhuatl, encuentra una oportunidad para resignificar su mundo y 
-            su propia persona”</h6>
-            <p class="card-text"><small class="text-muted">Sergio Sevilla</small></p>
-        </div>
-      </article>
-      <article class="card">
-        <img src="" class="card-img-top" alt="..." height="180;" style="background-color: aqua" >
-        <div class="card-txt text-justify">
-          <h6>Preservar las lenguas es indispensable ya que actualmente, al menos el 43% 
-            de las 6000 lenguas que se estima que se hablan en el mundo están en  peligro de extinción y se
-            calcula  que cada dos semanas desaparece una lengua que se lleva consigo todo un patrimonio 
-            cultural e intelectual.</h6>
-            <p class="card-text"><small class="text-muted">ONU</small></p>
-        </div>
-        
-      </article>
-      <article class="card">
-        <img src="" class="card-img-top" alt="..." height="180;"  style="background-color: aqua">
-        <div class="card-txt text-justify">
-          <h6>Por el interes en aprender náhuatl porque cree que es muy importante que 
-            en México las personas puedan tener un acercamiento con las lenguas indígenas que dice, 
-            deben ser motivo de orgullo.</h6>
-            <p class="card-text"><small class="text-muted">Flor es estudiante de Letras Hispánicas</small></p>
-        </div>
-      </article>
-    </div>
-  </section>
-  
-  
-</main>
-<br><br>
-<footer>
   
   <div class="redes">
     <h3>Seguinos en nuestras redes</h3>
@@ -124,7 +50,7 @@
       <li>TW</li>
     </ul>
   </div>
-</footer>
+
         
     
   
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -17,7 +17,7 @@
 	
 	<header>
     
-		<a ><img src="{{asset('img/logo.jpg')}}" width="60px"  alt="" href="" ><span style="color:white;">  CyberNahualt</span></a>
+		<a ><img src="{{asset('img/logoCyber.png')}}" width="60px"  alt="" href="" ><span style="color:white;">  CyberNahualt</span></a>
 		<nav>
       <a href="{{ route('login')}}"><span class="btn" style="background-color:#952f57; color:white">Ingresar</span></a>
       <a href="{{ route('auth.register')}}"><span class=" btn" style="background-color: #cd0c36; color:white">Registrar</span></a>
@@ -38,6 +38,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="{{ asset('js/navbar.js') }}"></script>
-    <script src="{{ asset('js/slider.js') }}"></script>
+
   </body>
 </html>
